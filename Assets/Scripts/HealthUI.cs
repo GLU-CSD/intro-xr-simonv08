afb63df30c153b536504b3ce6814ee31f4db329b
Simon Vastenhoud <180269184+simonv08@users.noreply.github.com>
2025-01-08 13:09:47 +0100

    added a playmap and finished mobile testing

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    public string prefabTag = "Enemy";
+    private Health HealthScript;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (HealthScript == null)
+        {
+            GameObject spawnedPrefab = GameObject.FindWithTag(prefabTag);
+            if (spawnedPrefab != null)
+            {
+                HealthScript = spawnedPrefab.GetComponent<Health>();
+            }
+
+        }
+    }
+
+    public void DamageButton()
+    {
+        if (HealthScript != null)
+        {
+            HealthScript.TakeDamage(10);
+        }
+    }
+
+    public void HealButton()
+    {
+        if (HealthScript != null)
+        {
+            HealthScript.RestoreHealth(10);
+        }
+    }
+}
