f5fc6f90ae6b4a306e30d4b644da74bb5aa6c7d8
Simon Vastenhoud <180269184+simonv08@users.noreply.github.com>
2025-01-09 19:31:11 +0100

    enemy movement,
    now create tower and spawner

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        // Vind de NavMeshAgent component
+        agent = GetComponent<NavMeshAgent>();
+
+        // Zoek de XR Rig met de tag "Player"
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerTransform = player.transform;
+        }
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        // Beweeg naar de positie van de speler als deze is gevonden
+        if (playerTransform != null)
+        {
+            agent.SetDestination(playerTransform.position);
+        }
+     
+    }
+}
