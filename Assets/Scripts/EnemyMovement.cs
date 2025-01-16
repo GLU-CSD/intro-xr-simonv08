de6260e089c12671073b050d38307da07a69c0c8
Simon Vastenhoud <180269184+simonv08@users.noreply.github.com>
2025-01-16 14:59:38 +0100

    added gun asset for lazer gun

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,11 +7,13 @@ public class EnemyMovement : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Transform BaseTransform;
+    private Health Health;
 
     void Start()
     {
         // Vind de NavMeshAgent component
         agent = GetComponent<NavMeshAgent>();
+        Health = GetComponent<Health>();
 
         // Zoek de XR Rig met de tag "Base"
         GameObject Base = GameObject.FindGameObjectWithTag("Base");
@@ -26,7 +28,7 @@ public class EnemyMovement : MonoBehaviour
     void Update()
     {
         // Beweeg naar de positie van de speler als deze is gevonden
-        if (BaseTransform != null  )
+        if (BaseTransform != null  && Health.currentHealth>0)
         {
             agent.SetDestination(BaseTransform.position);
         }
