b130764ee3aa91dc1c33e28ee707ded38f8514c4
Simon Vastenhoud <180269184+simonv08@users.noreply.github.com>
2025-01-23 18:36:49 +0100

    Revert "holster freeze position"
    This reverts commit 919a8f32c3a456de6c4e9b7c85ca2e6406ba9e90.

--- Assets/Scripts/HolsterMethod.cs
+++ Assets/Scripts/HolsterMethod.cs
@@ -9,23 +9,9 @@ public class HolsterMethod : MonoBehaviour
         if (other.CompareTag("Gun"))
         {
             other.transform.SetParent(transform);
-            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
-            rb.useGravity = false;
-            rb.velocity = Vector3.zero;
-            rb.freezeRotation = true;
+            other.gameObject.GetComponent<Rigidbody>().useGravity = false;
             Debug.Log("holstered");
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Gun"))
-        {
-            other.transform.SetParent(null);
-            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
-            rb.useGravity = true;
-            rb.freezeRotation = false;
-            Debug.Log("unholstered");
+            other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
     }
 }
